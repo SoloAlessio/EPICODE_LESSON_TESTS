4860e3f39b3c20987919f8bb9d6dbf844024cfcf
Alessio <belcastroalessio@outlook.it>
2023-06-26 21:31:30 +0200

    Update TEST.js

---
+++ TEST.js
@@ -0,0 +1,22 @@
+let frutto = "pera"
+
+switch (frutto){
+    case "mela": 
+        console.log("è una mela"); 
+        break;
+
+    case "pera": 
+        console.log("è una pera"); 
+        break;
+
+    case "banana": 
+        console.log("è una banana"); 
+        break;
+
+    case "caffe": 
+        console.log("è una caffe"); 
+        break;
+
+    default: 
+        console.log("non è un frutto");
+}
